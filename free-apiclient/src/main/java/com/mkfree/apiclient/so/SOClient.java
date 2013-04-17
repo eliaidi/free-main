3b4ed6f43fc6b61a9264a235976004ee0b4ad8c9
oyhk <oyhk@mkfree.com>
2013-04-17 20:26:11 +0800

    重整项目结构,把free-apiclient迁移到free-main,统一起来

---
+++ free-apiclient/src/main/java/com/mkfree/apiclient/so/SOClient.java
@@ -0,0 +1,52 @@
+package com.mkfree.apiclient.so;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+
+import com.mkfree.apithrift.ApiService;
+import com.mkfree.apithrift.BlogPostVO;
+
+public class SOClient {
+
+	public static void createIndex() {
+		try {
+			// 设置传输通道，对于非阻塞服务，需要使用TFramedTransport，它将数据分块发送
+			TTransport transport = new TFramedTransport(new TSocket("localhost", 9901));
+			transport.open();
+			TProtocol protocol = new TCompactProtocol(transport);// 使用高密度二进制协议
+			ApiService.Client client = new ApiService.Client(protocol);// 创建Client
+			long start = System.currentTimeMillis();
+			client.createIndex();
+			System.out.println("耗时：" + (System.currentTimeMillis() - start));
+			transport.close();// 关闭资源
+		} catch (TException x) {
+			x.printStackTrace();
+		}
+	}
+
+	public static List<BlogPostVO> search(String q) {
+		List<BlogPostVO> results = new ArrayList<BlogPostVO>();
+		try {
+			// 设置传输通道，对于非阻塞服务，需要使用TFramedTransport，它将数据分块发送
+			TTransport transport = new TFramedTransport(new TSocket("localhost", 9901));
+			transport.open();
+			TProtocol protocol = new TCompactProtocol(transport);// 使用高密度二进制协议
+			ApiService.Client client = new ApiService.Client(protocol);// 创建Client
+			long start = System.currentTimeMillis();
+			results = client.search(q);
+			System.out.println("耗时：" + (System.currentTimeMillis() - start));
+			transport.close();// 关闭资源
+			return results;
+		} catch (TException e) {
+			e.printStackTrace();
+		}
+		return results;
+	}
+}
---
+++ free-apiclient/src/main/java/com/mkfree/apiclient/sso/SSOClient.java
@@ -0,0 +1,40 @@
+package com.mkfree.apiclient.sso;
+
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+
+import com.mkfree.apithrift.ApiService;
+import com.mkfree.apithrift.SSOUserVO;
+
+/**
+ * sso 中心客户端类
+ * 
+ * @author hk
+ * 
+ *         2013-4-9 下午11:59:55
+ */
+public class SSOClient {
+
+	public static SSOUserVO login(String account, String password) {
+		SSOUserVO ssoUserVO = null;
+		try {
+			long start = System.currentTimeMillis();
+			// 设置传输通道， 对于非阻塞服务，需要使用TFramedTransport，它将数据分块发送
+			TTransport transport = new TFramedTransport(new TSocket("localhost", 9901));
+			transport.open();
+			TProtocol protocol = new TCompactProtocol(transport);// 使用高密度二进制协议
+			ApiService.Client client = new ApiService.Client(protocol);// 创建Client
+			ssoUserVO = client.loginByAccountAndPassword(account, password);
+			transport.close();// 关闭资源
+			System.out.println("耗时：" + (System.currentTimeMillis() - start));
+			return ssoUserVO;
+		} catch (TException x) {
+			x.printStackTrace();
+		}
+		return ssoUserVO;
+	}
+}
