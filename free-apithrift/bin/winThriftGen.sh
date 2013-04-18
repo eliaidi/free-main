49a2e8ea720ccedf1b7eae9d21116561c582eed2
oyhk <oyhk@mkfree.com>
2013-04-18 09:29:30 +0800

    modify api vo

---
+++ free-apithrift/bin/winThriftGen.sh
@@ -0,0 +1,2 @@
+thrift --gen java -out D:\\sofeware\\git_project\\free-main\\free-apithrift\\src\\main\\java D:\\sofeware\\git_project\\free-main\\free-apithrift\\src\\main\\resources\\base.thrift
+thrift --gen java -out D:\\sofeware\\git_project\\free-main\\free-apithrift\\src\\main\\java D:\\sofeware\\git_project\\free-main\\free-apithrift\\src\\main\\resources\\apiservice.thrift
