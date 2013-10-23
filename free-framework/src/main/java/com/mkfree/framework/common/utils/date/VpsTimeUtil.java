0b69719892342b80578814c901f8f47ce7f9d728
oyhk <381895649@qq.com>
2013-10-23 13:01:56 +0800

    修改服务器时间跟中国对应时间

--- free-framework/src/main/java/com/mkfree/framework/common/utils/date/VpsTimeUtil.java
+++ free-framework/src/main/java/com/mkfree/framework/common/utils/date/VpsTimeUtil.java
@@ -18,8 +18,8 @@ public class VpsTimeUtil {
 	 */
 	public static Date getVPSTime() {
 		Calendar calendar = Calendar.getInstance();
-		calendar.add(Calendar.HOUR, 15);
-		calendar.add(Calendar.MINUTE, 12);
+		calendar.add(Calendar.HOUR, 9);
+		calendar.add(Calendar.MINUTE, 11);
 		return calendar.getTime();
 	}
 }
