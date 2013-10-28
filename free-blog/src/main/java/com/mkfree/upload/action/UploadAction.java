53afca73313c233deb6d4a3488e6f3ddca8f074b
oyhk <381895649@qq.com>
2013-10-28 21:58:25 +0800

    整包结构

---
+++ free-blog/src/main/java/com/mkfree/upload/action/UploadAction.java
@@ -0,0 +1,52 @@
+package com.mkfree.upload.action;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.mkfree.apiclient.common.AppUploadAttrachmentClient;
+import com.mkfree.apithrift.vo.AppUploadAttachmentVO;
+import com.mkfree.blog.domain.AppUploadAttachment;
+import com.mkfree.framework.common.constants.AppUploadConstants;
+import com.mkfree.framework.common.spring.CallBackJson;
+import com.mkfree.framework.common.web.request.RequestUtils;
+
+@Controller
+public class UploadAction {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(UploadAction.class);
+
+	@RequestMapping(value = "/uploadInit", method = RequestMethod.GET)
+	public String uploadInit() {
+		return "blog/test_upload";
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/upload/blog-post", method = RequestMethod.POST)
+	public CallBackJson uploadBlogPost(HttpServletRequest request, @RequestParam(value = "uploadfile", required = true) MultipartFile uploadfile, AppUploadAttachment attachment) {
+		CallBackJson json = new CallBackJson();
+		try {
+			String fileName = new String(uploadfile.getOriginalFilename().getBytes("iso-8859-1"), "UTF-8");
+			String userIp = RequestUtils.getIpAddr(request);
+			AppUploadAttachmentVO vo = AppUploadAttrachmentClient.saveAppUploadAttachment(ByteBuffer.wrap(uploadfile.getBytes()), AppUploadConstants.BLOG_POST_APPNAME, fileName,
+					attachment.getUserId(), userIp);
+			json.setObj(vo);
+			json.setSuccess(true);
+		} catch (IllegalStateException | IOException e) {
+			logger.error("uploadBlogPost() - upload exception...", e); //$NON-NLS-1$
+		}
+		return json;
+	}
+}
