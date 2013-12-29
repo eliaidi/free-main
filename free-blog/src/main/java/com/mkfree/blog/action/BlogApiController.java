64a5e255f5001cb9231758a75f1c8f5a3db052aa
oyhk <381895649@qq.com>
2013-12-29 21:28:19 +0800

    优化api接口需要字段

--- free-blog/src/main/java/com/mkfree/blog/action/BlogApiController.java
+++ free-blog/src/main/java/com/mkfree/blog/action/BlogApiController.java
@@ -1,5 +1,8 @@
 package com.mkfree.blog.action;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mkfree.apiclient.blog.BlogClient;
+import com.mkfree.apithrift.vo.BlogPostVO;
 import com.mkfree.apithrift.vo.PaginationVO;
 import com.mkfree.framework.common.spring.CallBackJson;
 
@@ -34,8 +38,17 @@ public class BlogApiController {
 		long start = System.currentTimeMillis();
 		PaginationVO pages = BlogClient.getPage(pageNo, 15);
 		CallBackJson json = new CallBackJson();
-		pages.getDatas();
-		json.setObj(pages.getDatas());
+		List<BlogPostVO> blogPostVOs = pages.getDatas();
+		// 暂时这样优化api接口,获取需要的字段
+		List<BlogPostVO> datas = new ArrayList<BlogPostVO>();
+		for (int i = 0; i < blogPostVOs.size(); i++) {
+			BlogPostVO blogPostVO = new BlogPostVO();
+			blogPostVO.setId(blogPostVOs.get(i).getId());
+			blogPostVO.setTitle(blogPostVOs.get(i).getTitle());
+			blogPostVO.setSummary(blogPostVOs.get(i).getSummary());
+			datas.add(blogPostVO);
+		}
+		json.setObj(datas);
 		json.setSuccess(true);
 		System.out.println(System.currentTimeMillis() - start);
 		return json;
